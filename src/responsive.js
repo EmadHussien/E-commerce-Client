3382dd4545a526f9e05d37358895bc403070f279
Emad Hussien <emadhussien851@gmail.com>
2023-11-25 23:45:47 +0200

    Update responsive.js

--- src/responsive.js
+++ src/responsive.js
@@ -2,7 +2,7 @@ import { css } from "styled-components";
 
 export const mobile = (styles) => {
   return css`
-    @media only screen and (max-width: 410px) {
+    @media only screen and (max-width: 420px) {
       ${styles}
     }
   `;
